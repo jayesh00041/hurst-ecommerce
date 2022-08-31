17c5ae929c1b42e552b8c8ac4c95a90d0bb5a87a
jayesh00041 <jayesh00041@gmail.com>
2022-08-31 14:14:16 +0530

    Add files via upload

---
+++ precache-manifest.da8505ccffbc7d04bddbaf359f293140.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0351d373f0e6ccf08d0",
+    "url": "/react-ecommerce/static/css/main.e6673596.chunk.css"
+  },
+  {
+    "revision": "b0351d373f0e6ccf08d0",
+    "url": "/react-ecommerce/static/js/main.b0351d37.chunk.js"
+  },
+  {
+    "revision": "079a51e1eca575ceb4bf",
+    "url": "/react-ecommerce/static/js/1.079a51e1.chunk.js"
+  },
+  {
+    "revision": "e698f7258b448489c234",
+    "url": "/react-ecommerce/static/css/2.dee2b268.chunk.css"
+  },
+  {
+    "revision": "e698f7258b448489c234",
+    "url": "/react-ecommerce/static/js/2.e698f725.chunk.js"
+  },
+  {
+    "revision": "4dba3b24a18716b2f75d",
+    "url": "/react-ecommerce/static/js/runtime~main.4dba3b24.js"
+  },
+  {
+    "revision": "976d397d6b3b5a74d98b733e7a432153",
+    "url": "/react-ecommerce/index.html"
+  }
+];
